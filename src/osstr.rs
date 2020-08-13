4af496dacb26d87da202e7186f9e14e8ef578b22
Ian Boll <iangboll@vt.edu>
2020-08-13 19:47:03 -0400

    added LICENSE and other info for crates.io

--- src/osstr.rs
+++ src/osstr.rs
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ *  * License, v. 2.0. If a copy of the MPL was not distributed with this
+ *   * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
 use std::ffi::OsStr;
 
 /// Helper methods for working with OsStr (and OsString which `Deref`s to it).
